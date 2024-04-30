0927f66515b08b8253c7002f83983b1a8cf0765e
LeoChen <leo_chen@mayohr.com>
2024-04-30 16:28:31 +0800

    add theme config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: 'rgba(var(--background))',
+				border: 'rgba(var(--border))',
+				card: 'rgba(var(--card))',
+				'copy-primary': 'rgba(var(--copy-primary))',
+				'copy-secondary': 'rgba(var(--copy-secondary))',
+				cta: 'rgba(var(--cta))',
+				'cta-active': 'rgba(var(--cta-active))',
+				'cta-text': 'rgba(var(--cta-text))',
+				// common
+				grape: 'rgba(var(--grape))',
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
